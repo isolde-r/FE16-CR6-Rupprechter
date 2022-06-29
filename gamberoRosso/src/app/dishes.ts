5ac49dc43faf7b0c9b7663dff8680ce4f3908c28
isolde-r <writetoisolde@gmail.com>
2022-06-29 13:29:39 +0200

    change links

--- gamberoRosso/src/app/dishes.ts
+++ gamberoRosso/src/app/dishes.ts
@@ -20,10 +20,10 @@ export class Dish {
 
 export const dishes: Dish[] = []
 
-let vitello = new Dish("Vitello Tonnato", "/assets/img/vitello.jpg", "An all time classic of italian cuisine, seasoned with a spruce of lemon and sicilian capers", 14.99, true, ["meat", "fish"], ["veal", "capers", "anchovies", "tuna", "eggs"]);
-let branzino = new Dish("Branzino ripieno", "/assets/img/branzino.jpg", "Grilled branzino, stuffed with tomatoes and onion", 18.99, false, ["vegeterian", "fish"], ["sea bass", "tomatoes", "onion"]);
-let spaghetti = new Dish("Spaghetti mollicati", "/assets/img/spaghetti.jpg", "A dish of farmers tradition taken to the highest level of cuisine. You don't know what a bunch of simple bread crumb can add to the taste, until you tried this. ", 10.99, true, ["gluten", "vegeterian"], ["spaghetti", "bread crumbs", "anchovies"]);
-let carciofi = new Dish("Carciofi alla griglia", "/assets/img/carciofi.jpg", "Sometimes less is more: apulian artichokes seasoned with lemon, mediterranean herbs and lots of garlic. Take the charcoal grill for granted.", 16.99, false, ["vegeterian"], ["artichokes", "lemon", "garlic"]);
-let pannaCotta = new Dish("Panna Cotta", "/assets/img/panna_cotta.jpg", "Maybe the most famous of all italian desserts, beyond tiramisú. At Gambero Rosso you'll get the tricolore version of it. Meaning everytime with red fruits, according to the season.", 7.99, true, ["lactose"], ["cream", "vanilla bean", "gelatin"]);
-let polenta = new Dish("Polenta con ragú", "/assets/img/polenta.jpg", "We take it easy beeing \"polentoni\". To be honest, this typical north-italian dish is a culinary delight.", 12.99, true, ["meat"], ["polenta", "pancetta bacon", "beef", "tomatoes"]);
+let vitello = new Dish("Vitello Tonnato", "./assets/img/vitello.jpg", "An all time classic of italian cuisine, seasoned with a spruce of lemon and sicilian capers", 14.99, true, ["meat", "fish"], ["veal", "capers", "anchovies", "tuna", "eggs"]);
+let branzino = new Dish("Branzino ripieno", "./assets/img/branzino.jpg", "Grilled branzino, stuffed with tomatoes and onion", 18.99, false, ["vegeterian", "fish"], ["sea bass", "tomatoes", "onion"]);
+let spaghetti = new Dish("Spaghetti mollicati", "./assets/img/spaghetti.jpg", "A dish of farmers tradition taken to the highest level of cuisine. You don't know what a bunch of simple bread crumb can add to the taste, until you tried this. ", 10.99, true, ["gluten", "vegeterian"], ["spaghetti", "bread crumbs", "anchovies"]);
+let carciofi = new Dish("Carciofi alla griglia", "./assets/img/carciofi.jpg", "Sometimes less is more: apulian artichokes seasoned with lemon, mediterranean herbs and lots of garlic. Take the charcoal grill for granted.", 16.99, false, ["vegeterian"], ["artichokes", "lemon", "garlic"]);
+let pannaCotta = new Dish("Panna Cotta", "./assets/img/panna_cotta.jpg", "Maybe the most famous of all italian desserts, beyond tiramisú. At Gambero Rosso you'll get the tricolore version of it. Meaning everytime with red fruits, according to the season.", 7.99, true, ["lactose"], ["cream", "vanilla bean", "gelatin"]);
+let polenta = new Dish("Polenta con ragú", "./assets/img/polenta.jpg", "We take it easy beeing \"polentoni\". To be honest, this typical north-italian dish is a culinary delight.", 12.99, true, ["meat"], ["polenta", "pancetta bacon", "beef", "tomatoes"]);
 
--- gamberoRosso/src/app/order/order.component.ts
+++ gamberoRosso/src/app/order/order.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from '../order.service';
-import { Dish } from '../dishes';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Dish, dishes } from '../dishes';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'order',
@@ -10,22 +10,45 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class OrderComponent implements OnInit {
   cartItems: Dish[] = [];
+  plate: Dish = {} as Dish;
+  index: number = 0;
   totalProd: number = 0;
   totalSum: number = 0;
   service: number = 0;
   discount: number = 0;
-  constructor(private orderServ: OrderService) { }
+  constructor(private orderServ: OrderService) { };
 
   checkOutForm = new FormGroup({
-    name: new FormControl(""),
-    address: new FormControl(""),
+    name: new FormControl("", Validators.required),
+    address: new FormControl("", Validators.required),
     phone: new FormControl(""),
-    email: new FormControl("")
-  })
+    email: new FormControl(""),
+  });
+
 
   onPurchase() {
+    const alertItems = [];
+    const tempCartContent = this.orderServ.items;
+    for (const item of tempCartContent) {
+
+      let items = item.name;
+      alertItems.push(items);
+    }
+
+
+
     this.cartItems = this.orderServ.clearCart();
     this.checkOutForm.reset();
+
+    if (this.checkOutForm.valid) {
+      // window.alert(`Thank you for your order. You purchased the following products:\n${alertItems.join("\n")}.\nThe dishes will be delievered to the following adress:`);
+      var a: any = this.checkOutForm.value;
+
+      console.log(a)
+
+    } else {
+      window.alert(`Some of the order data are not correct, please try again.`)
+    }
   }
 
   ngOnInit(): void {
